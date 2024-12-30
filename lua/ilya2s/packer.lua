40291e64b3118006f60d918d8ef027278eb11a47
Ilyass El Ouazzani <163026421+ilya2sel@users.noreply.github.com>
2024-12-30 16:29:41 -0500

    Update packer.lua

--- lua/ilya2s/packer.lua
+++ lua/ilya2s/packer.lua
@@ -1,84 +1,74 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
+-- [[ Packer Setup ]]
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
+    -- [[ Packer can manage itself ]]
     use 'wbthomason/packer.nvim'
 
+    -- [[ Telescope ]]
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        requires = {{ 'nvim-lua/plenary.nvim' }}
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        requires = { 'nvim-lua/plenary.nvim' }
     }
 
-    use({
+    -- [[ Colorscheme ]]
+    use {
         'rose-pine/neovim',
         as = 'rose-pine',
         config = function()
             vim.cmd('colorscheme rose-pine')
         end
-    })
-
-    use({
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                icons = false,
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    })
-
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('nvim-treesitter/playground')
-    use("nvim-treesitter/nvim-treesitter-context");
-
-    use {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        requires = { { "nvim-lua/plenary.nvim" } }
     }
 
-    use("mbbill/undotree")
-
-    use('tpope/vim-fugitive')
-
+    -- [[ LSP and Autocompletion ]]
     use {
         'vonheikemen/lsp-zero.nvim',
         branch = 'v3.x',
         requires = {
-            -- lsp support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            -- LSP Support
+            'neovim/nvim-lspconfig',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
 
-            -- autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+            -- Autocompletion
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
 
-            -- snippets
-            { 'l3mon4d3/luasnip' },
-            { 'rafamadriz/friendly-snippets' },
+            -- Snippets
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
         }
     }
 
+    -- [[ Treesitter ]]
     use {
-        "windwp/nvim-autopairs",
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
     }
+    use 'nvim-treesitter/playground'
+    use 'nvim-treesitter/nvim-treesitter-context'
 
-    use("github/copilot.vim")
-    use("eandrju/cellular-automaton.nvim")
+    -- [[ Utilities ]]
+    use {
+        'ThePrimeagen/harpoon',
+        branch = "harpoon2",
+        requires = 'nvim-lua/plenary.nvim'
+    }
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
+    use 'folke/trouble.nvim'
+    use 'windwp/nvim-autopairs'
+    use 'github/copilot.vim'
+    use 'eandrju/cellular-automaton.nvim'
 
-    use({
-        "jose-elias-alvarez/null-ls.nvim",
-        requires = {"nvim-lua/plenary.nvim"}
-    })
-    use 'mfussenegger/nvim-jdtls'
+    -- [[ Null-ls ]]
+    use {
+        'jose-elias-alvarez/null-ls.nvim',
+        requires = 'nvim-lua/plenary.nvim'
+    }
 end)
