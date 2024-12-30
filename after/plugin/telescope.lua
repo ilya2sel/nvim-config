9db0f4dd3a71051286e9f0882d4ece5abe82560b
Ilyass El Ouazzani <163026421+ilya2sel@users.noreply.github.com>
2024-12-30 16:27:47 -0500

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find files" })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Git files" })
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Help tags" })
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+end, { desc = "Search string" })
