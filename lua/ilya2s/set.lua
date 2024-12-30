b5fcd7844f227abf2801d0f773b75b3d56b1bd52
Ilyass El Ouazzani <163026421+ilya2sel@users.noreply.github.com>
2024-12-30 16:30:22 -0500

    Update set.lua

--- lua/ilya2s/set.lua
+++ lua/ilya2s/set.lua
@@ -1,37 +1,33 @@
--- line numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
+-- [[ Basic Settings ]]
+vim.opt.nu = true                        -- Line numbers
+vim.opt.relativenumber = true            -- Relative line numbers
 
--- indentation
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+-- [[ Indentation ]]
+vim.opt.tabstop = 4                      -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 4                  -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.shiftwidth = 4                   -- Size of an indent
+vim.opt.expandtab = true                 -- Use spaces instead of tabs
+vim.opt.smartindent = true               -- Smart indentation
 
--- line wrap
-vim.opt.wrap = false
+-- [[ Line Wrapping ]]
+vim.opt.wrap = false                     -- Disable line wrap
 
--- long running undos
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+-- [[ Backup and Undo ]]
+vim.opt.swapfile = false                 -- Disable swapfile
+vim.opt.backup = false                   -- Disable backup
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"  -- Set undo directory
+vim.opt.undofile = true                  -- Enable persistent undo
 
--- incremental search
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+-- [[ Search ]]
+vim.opt.hlsearch = false                 -- Disable highlight on search
+vim.opt.incsearch = true                 -- Enable incremental search
 
--- good colors
-vim.opt.termguicolors = true
+-- [[ Appearance ]]
+vim.opt.termguicolors = true             -- True color support
+vim.opt.scrolloff = 8                    -- Minimum number of screen lines to keep above and below the cursor
+vim.opt.signcolumn = "yes"               -- Always show the sign column
+vim.opt.isfname:append("@-@")            -- Treat '@' as part of a file name
+vim.opt.colorcolumn = "80"               -- Highlight column at 80 characters
 
--- scroll off
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
--- fast update time
-vim.opt.updatetime = 50
-
--- Mark 80 chars column
-vim.opt.colorcolumn = "80"
+-- [[ Performance ]]
+vim.opt.updatetime = 50                  -- Faster completion
